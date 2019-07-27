5b194350e74585281556185ee7002a816de51904
sthgreat <284387885@qq.com name = sthgreat>
2019-07-27 18:44:11 +0800

    添加新文件

---
+++ src/main/java/dzkjdx/jsb/web_community/WebCommunityApplication.java
@@ -0,0 +1,13 @@
+package dzkjdx.jsb.web_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebCommunityApplication.class, args);
+    }
+
+}
