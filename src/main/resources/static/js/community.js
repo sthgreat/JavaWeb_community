9f7f581fd9aa2c4f26089f550e02829c1339eab0
sthgreat <284387885@qq.com>
2019-08-26 10:25:28 +0800

    未登陆时跳转

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -11,10 +11,19 @@ function post() {
            "type":1
        }),
        success:function (response) {
-           if(response.code===200){
+           debugger;
+           if(response.code==200){
                $("#comment_section").hide();
            }else {
-               alert(response.message);
+               if(response.code==2003){
+                   var isAccepted = confirm(response.message);
+                   if(isAccepted){
+                       window.open("https://github.com/login/oauth/authorize?client_id=93c26d7d2df671df7b7a&redirect_uri=http://localhost:8080/callback&scope=user&state=1");
+                       window.localStorage.setItem("closable",true);
+                   }
+               }else {
+                   alert(response.message);
+               }
            }
            console.log(response)
        } ,
